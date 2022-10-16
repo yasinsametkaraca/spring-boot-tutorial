fe5d498db1642b53c8c85493a5cd251f2764486f
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2022-10-16 11:48:47 +0300

    fixed

--- northwind/src/main/java/com/ysk/northwind/NorthwindApplication.java
+++ northwind/src/main/java/com/ysk/northwind/NorthwindApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NortwindApplication {
+public class NorthwindApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NortwindApplication.class, args);
+		SpringApplication.run(NorthwindApplication.class, args);
 	}
 
 }
